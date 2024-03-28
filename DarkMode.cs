acc6df73ba7477aee948dfa130c4e1338f2885f7
petercsakany <peter.bootmaker@gmail.com>
2024-03-28 12:26:31 +0000

    Add project files.

---
+++ DarkMode.cs
@@ -0,0 +1,66 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace CashRegisterDarkTheme
+{
+    public class Darkmode : MelonMod
+    {
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            if (buildIndex == 1)
+            {
+                GameObject checkoutManager = GameObject.Find("---MANAGERS---/Checkout Manager");
+                if (checkoutManager != null)
+                {
+                    foreach (Transform checkout in checkoutManager.transform)
+                    {
+                        if (checkout.name.StartsWith("Checkout"))
+                        {
+                            Transform cashRegister = checkout.Find("Cash Register");
+                            if (cashRegister != null)
+                            {
+                                SetColorToComponent(cashRegister.Find("Screen/BG")?.gameObject, new Color(0.208f, 0.216f, 0.294f));
+                                SetColorToComponent(cashRegister.Find("Screen/Title/BG")?.gameObject, new Color(0.204f, 0.286f, 0.333f));
+                                SetColorToComponent(cashRegister.Find("Screen/Title/Title")?.gameObject, new Color(0.004f, 0.612f, 0.741f));
+                                SetColorToComponent(cashRegister.Find("Screen/Title/Icon")?.gameObject, new Color(0.004f, 0.612f, 0.741f));
+                                SetColorToComponent(cashRegister.Find("Screen/Info Panel")?.gameObject, new Color(0.314f, 0.447f, 0.482f));
+                                SetColorToComponent(cashRegister.Find("Screen/Info Panel/Divider")?.gameObject, new Color(0.004f, 0.612f, 0.741f));
+                                SetColorToComponent(cashRegister.Find("Screen/Change Panel")?.gameObject, new Color(0.204f, 0.286f, 0.333f));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        void SetColorToComponent(GameObject go, Color color)
+        {
+            if (go == null) return;
+            var image = go.GetComponent<Image>();
+            if (image != null)
+            {
+                image.color = color;
+                return;
+            }
+            var rawImage = go.GetComponent<RawImage>();
+            if (rawImage != null)
+            {
+                rawImage.color = color;
+                return;
+            }
+            var textMeshPro = go.GetComponent<TextMeshProUGUI>();
+            if (textMeshPro != null)
+            {
+                textMeshPro.color = color;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using MelonLoader;
+using CashRegisterDarkTheme; // The namespace of your mod class
+// ...
+[assembly: MelonInfo(typeof(Darkmode), "Cash Register Dark Theme", "1.0.1", "Morabis")]
+[assembly: MelonGame("Nokta Games", "Supermarket Simulator")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CashRegisterDarkTheme")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CashRegisterDarkTheme")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0a3abe64-c03c-441a-8004-cd7672d60999")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
